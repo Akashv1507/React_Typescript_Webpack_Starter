ad17acb5faec57a87aa0850b57d691f606c13b61
Aakash Verma <akash.v@wrldc.local>
2021-10-27 17:24:13 +0530

    backend folder structure done

--- Back_End/server.py
+++ Back_End/server.py
@@ -1,13 +1,26 @@
-from src.appConfig import loadAppConfig
-from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
+# 
+# from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
+# from waitress import serve
+
+# app = Flask(__name__)
+
+# # get application config
+# configDict = loadAppConfig()
+
+# if __name__ == '__main__':
+#     serverMode: str = configDict['mode']
+#     if serverMode.lower() == 'd':
+#         app.run(host="localhost", port=int(configDict['flaskPort']), debug=True)
+#     else:
+#         serve(app, host='0.0.0.0', port=int(
+#             configDict['flaskPort']), threads=1)
+from core import app
+from core.appConfig import getAppConfigDict
 from waitress import serve
 
-app = Flask(__name__)
+configDict = getAppConfigDict()
 
-# get application config
-configDict = loadAppConfig()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     serverMode: str = configDict['mode']
     if serverMode.lower() == 'd':
         app.run(host="localhost", port=int(configDict['flaskPort']), debug=True)
